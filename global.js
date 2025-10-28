bb4a9387bedd82815f4937a776fd841f12dc2743
Huzaima007 <160530387+Huzaima007@users.noreply.github.com>
2025-10-28 13:52:07 +0500

    Create global.js

---
+++ global.js
@@ -0,0 +1,21 @@
+// Global Object 
+
+
+// console.log(global);
+
+
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('in the timeout');
+// }, 1000)
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+// console.log(document.querySelector);
